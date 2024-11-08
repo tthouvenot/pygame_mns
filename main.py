594d890315e167551132879ee429f6ac8b02bf81
Tristan Thouvenot <tristan.thouvenot57@gmail.com>
2024-11-08 09:47:01 +0100

    feature: moving ball

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Aller-Retour d'une Balle")
+pygame.display.set_caption("Bomberman")
 
 # Couleurs
 rouge = (255, 0, 0)
@@ -19,6 +19,12 @@ white = (255, 255, 255)
 en_cours = True
 clock = pygame.time.Clock()
 
+x_balle = 20
+vitesse_x = 5
+y_balle = 20
+vitesse_y = 5
+
+
 while en_cours:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,7 +32,15 @@ while en_cours:
 
     # Remplir l'écran et dessiner la balle
     screen.fill(white)
-    pygame.draw.circle(screen, rouge, (100, 200), 20)
+    pygame.draw.circle(screen, rouge, (x_balle, y_balle), 20) # (origine, couleur, (posX, posY), rayon)
+
+    x_balle += vitesse_x
+    y_balle += vitesse_y
+    
+    if x_balle > 800 - 20 or x_balle <= 20 :
+        vitesse_x = -vitesse_x
+    elif y_balle > 600 - 20 or y_balle <= 20:
+        vitesse_y = -vitesse_y 
 
     # Rafraîchir l'affichage
     pygame.display.flip()
